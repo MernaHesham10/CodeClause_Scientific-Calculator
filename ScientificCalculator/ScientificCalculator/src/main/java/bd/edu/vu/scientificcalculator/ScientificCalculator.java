d72ce153de5737a3473a3f2763d2cdb75d41012d
MernaHesham10 <mernahesham21010@gmail.com>
2022-10-13 21:17:15 +0200

    first commit

--- ScientificCalculator/ScientificCalculator/src/main/java/bd/edu/vu/scientificcalculator/ScientificCalculator.java
+++
@@ -1,8 +0,0 @@
-package bd.edu.vu.scientificcalculator;
-
-public class ScientificCalculator {
-    public static void main(String[] args) {
-        welcome w = new welcome();
-        w.setVisible(true);
-    }
-}
